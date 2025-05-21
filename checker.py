9ed33513889d453ad28dfcd49204c5de20974e33
inkied <wquxbl@icloud.com>
2025-05-20 20:08:03 -0400

    Update checker.py

--- checker.py
+++ checker.py
@@ -68,4 +68,5 @@ async def webhook(request: Request):
     return {"ok": True}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
